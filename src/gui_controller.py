d420184e6f9fe4bd20e1c97c5a94698f82b7a379
aninisrinivasan <aninisrinivasan@gmail.com>
2024-02-11 18:09:02 +0000

    Iteration1: Changed the template files path ad relative

--- src/gui_controller.py
+++ src/gui_controller.py
@@ -1,13 +1,19 @@
+import os
 from PyQt5.QtWidgets import QApplication
-
 from renderer import UIRender
 
 if __name__ == "__main__":
     app = QApplication([])
 
-    # Specify the paths to your HTML and CSS files
-    html_file_path = "/Users/aninisrinivasan/PycharmProjects/AITextCompare/template/diff_output.html"
-    css_file_path = "/Users/aninisrinivasan/PycharmProjects/AITextCompare/template/styles.css"
+    # Get the directory of the current script
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Get the parent directory of the current directory
+    parent_directory = os.path.dirname(current_directory)
+
+    # Specify the relative paths to your HTML and CSS files
+    html_file_path = os.path.join(parent_directory, "template", "diff_output.html")
+    css_file_path = os.path.join(parent_directory, "template", "styles.css")
 
     renderer = UIRender(html_file_path, css_file_path)  # Pass HTML and CSS file paths as positional arguments
     renderer.show()
