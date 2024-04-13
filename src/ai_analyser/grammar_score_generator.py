1ac0d11eba08f255437b4b3b7d20993c4a3819d0
aninisrinivasan <aninisrinivasan@gmail.com>
2024-04-13 22:16:26 +0100

    code refactoring

--- src/ai_analyser/grammar_score_generator.py
+++ src/ai_analyser/grammar_score_generator.py
@@ -1,8 +1,4 @@
-import torch
-from transformers import BertTokenizer, BertForMaskedLM
-from spellchecker import SpellChecker
 from transformers import pipeline
-
 from difflib import SequenceMatcher
 
 corrector = pipeline("text2text-generation", model="pszemraj/flan-t5-large-grammar-synthesis")
@@ -10,11 +6,6 @@ corrector = pipeline("text2text-generation", model="pszemraj/flan-t5-large-gramm
 
 class GrammarScoreGenerator:
 
-    @staticmethod
-    def get_similarity_score(original_sentence, corrected_sentence):
-        # Calculate the grammar score as the percentage of similarity between the two sentences
-        grammar_score = 100 - (abs(len(original_sentence) - len(corrected_sentence)) / len(original_sentence) * 100)
-        return int(grammar_score)
 
     @staticmethod
     # Function to calculate the grammar score for a given sentence
@@ -22,7 +13,6 @@ class GrammarScoreGenerator:
         corrected_sentence = corrector(new_content)[0]["generated_text"]
 
         print("corrected_sentence : " + corrected_sentence)
-        # grammar_score = GrammarScoreGenerator.get_similarity_score(new_content, corrected_sentence)
         grammar_score = GrammarScoreGenerator.calculate_grammar_score(new_content, corrected_sentence)
 
         return GrammarScoreGenerator.get_html(int(grammar_score))
